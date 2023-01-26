aef4a7f3d25a3d1b0688a19010931fcdb82c0a6c
Rodrigo Lins <rlinsdev@gmail.com>
2023-01-26 15:01:52 -0600

    Add Category

--- Plugins.DataStore.InMemory/CategoryInMemoryRepository.cs
+++ Plugins.DataStore.InMemory/CategoryInMemoryRepository.cs
@@ -21,6 +21,16 @@ namespace Plugins.DataStore.InMemory
 				new Category {CategoryId = 1, Name = "Meat", Description = "Meat" },
 			};
 		}
+
+		public void AddCategory(Category category)
+		{
+			if (categories.Any(x => x.Name.Equals(category.Name, StringComparison.OrdinalIgnoreCase)))
+				return;
+			var id = categories.Max(x => x.CategoryId);
+			category.CategoryId = id + 1;
+			categories.Add(category);
+		}
+
 		public IEnumerable<Category> GetCategories()
 		{
 			return categories;
