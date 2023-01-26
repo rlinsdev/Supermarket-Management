722987e8cc58fc809c54b2a48a0e4e80080bd46a
Rodrigo Lins <rlinsdev@gmail.com>
2023-01-25 19:52:41 -0600

    CategoryUseCase

--- SupermarketManagement/UseCases/ViewCategoriesUseCase.cs
+++ SupermarketManagement/UseCases/ViewCategoriesUseCase.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using CoreBusiness;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
 
 namespace UseCases
 {
 	public class ViewCategoriesUseCase
 	{
-		public IEnumerable<Categorie> Execute()
+		public ViewCategoriesUseCase(ICategoryRepository categoryRepository)
+		{
+			CategoryRepository = categoryRepository;
+		}
+
+		public ICategoryRepository CategoryRepository { get; }
+
+		public IEnumerable<Category> Execute()
+		{
+			return CategoryRepository.GetCategories();
+		}
 	}
 }
