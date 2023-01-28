064c79d38e708659e2fb5a7f3ac4c0815b1e2faa
Rodrigo Lins <rlinsdev@gmail.com>
2023-01-28 15:53:35 -0300

    controll - ProductsForSelling

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,15 +1,9 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Plugins.DataStore.InMemory;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using UseCases;
 using UseCases.DataStorePluginInterfaces;
 using WebApp.Data;
@@ -48,6 +42,7 @@ namespace WebApp
 			services.AddTransient<IEditProductUseCase, EditProductUseCase>();
 			services.AddTransient<IGetProductByIdUseCase, GetProductByIdUseCase>();
 			services.AddTransient<IDeleteProductUseCase, DeleteProductUseCase>();
+			services.AddTransient<IViewProductsByCategory, ViewProductsByCategory>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
