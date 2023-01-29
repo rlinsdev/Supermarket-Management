6eabe221397f05dacb8d8fdc51d084b10364fa17
Rodrigo Lins <rlinsdev@gmail.com>
2023-01-29 09:13:53 -0300

    Transactions

---
+++ UseCases/RecordTransactionUseCase.cs
@@ -0,0 +1,24 @@
+﻿using UseCases.DataStorePluginInterfaces;
+
+namespace UseCases
+{
+	public class RecordTransactionUseCase
+	{
+		private readonly ITransactionRepository transactionRepository;
+		private readonly IGetProductByIdUseCase getProductByIdUseCase;
+
+		public RecordTransactionUseCase(
+			ITransactionRepository TransactionRepository,
+			IGetProductByIdUseCase GetProductByIdUseCase)
+		{
+			transactionRepository = TransactionRepository;
+			getProductByIdUseCase = GetProductByIdUseCase;
+		}
+
+		public void Execute(int productId, int qty)
+		{
+			var price = getProductByIdUseCase.Execute(productId).Price;
+			transactionRepository.Save(productId, price, qty);
+		}
+	}
+}
