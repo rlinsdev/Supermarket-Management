033d727a2a91fbdcb8ddb15cc9f78764aa2148b0
Rodrigo Lins <rlinsdev@gmail.com>
2023-01-29 08:46:46 -0300

    Sell product

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -43,6 +43,7 @@ namespace WebApp
 			services.AddTransient<IGetProductByIdUseCase, GetProductByIdUseCase>();
 			services.AddTransient<IDeleteProductUseCase, DeleteProductUseCase>();
 			services.AddTransient<IViewProductsByCategory, ViewProductsByCategory>();
+			services.AddTransient<ISellProductUseCase, SellProductUseCase>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
